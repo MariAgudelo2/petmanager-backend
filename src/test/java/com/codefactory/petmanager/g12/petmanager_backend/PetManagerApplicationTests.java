4b93de2fe349c391546ed31e7c809c76332997dd
MariAgudelo2 <150271644+MariAgudelo2@users.noreply.github.com>
2025-09-14 17:40:28 -0500

    Inicializando proyecto PetManager Backend

---
+++ src/test/java/com/codefactory/petmanager/g12/petmanager_backend/PetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefactory.petmanager.g12.petmanager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
