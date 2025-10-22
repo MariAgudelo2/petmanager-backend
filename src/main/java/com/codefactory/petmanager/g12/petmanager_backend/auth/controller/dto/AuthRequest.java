0f33f27871afd34bad3dbbb6f758ecb0019cf330
FelipeUH <f.uribe@udea.edu.co>
2025-10-22 10:43:00 -0500

    feature: added validation to auth requests

--- src/main/java/com/codefactory/petmanager/g12/petmanager_backend/auth/controller/dto/AuthRequest.java
+++ src/main/java/com/codefactory/petmanager/g12/petmanager_backend/auth/controller/dto/AuthRequest.java
@@ -1,10 +1,19 @@
 package com.codefactory.petmanager.g12.petmanager_backend.auth.controller.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AuthRequest {
+    @Email(message = "Email must be valid")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", 
+        message = "Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, one number, and one special character (@, $, %, !, #, ?)")
     private String password;
 }
 
--- src/main/java/com/codefactory/petmanager/g12/petmanager_backend/user/controller/dto/UserRequestDTO.java
+++ src/main/java/com/codefactory/petmanager/g12/petmanager_backend/user/controller/dto/UserRequestDTO.java
@@ -3,7 +3,7 @@ package com.codefactory.petmanager.g12.petmanager_backend.user.controller.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,12 +14,14 @@ import lombok.NoArgsConstructor;
 public class UserRequestDTO {
   
   @NotBlank(message = "ID number is required")
+  @Pattern(regexp = "^[0-9]{6,12}$", message = "Document must be between 6 and 12 digits and contain only numbers")
   private String idNumber;
   
   @NotBlank(message = "ID type is required")
   private String idType;
   
   @NotBlank(message = "Name is required")
+  @Pattern(regexp = "^(?=.*\\s)([A-Za-záéíóúÁÉÍÓÚñÑ\\s]{5,100})$", message = "Name must have at least two words, between 5 and 100 characters, and contain only letters and spaces")
   private String name;
   
   @NotNull(message = "Role ID is required")
@@ -33,7 +35,8 @@ public class UserRequestDTO {
   private String email;
   
   @NotBlank(message = "Password is required")
-  @Size(min = 8, message = "Password must be at least 8 characters long")
+  @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", 
+           message = "Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, one number, and one special character (@, $, %, !, #, ?)")
   private String password;
   
   private boolean isActive = true;
