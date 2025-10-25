dc3f3d655b879038bd144a4894b165c0bbd3dd28
FelipeUH <f.uribe@udea.edu.co>
2025-10-25 16:33:13 -0500

    remove unused import

--- src/main/java/com/codefactory/petmanager/g12/petmanager_backend/supplier/controller/dto/SupplierRequestDTO.java
+++ src/main/java/com/codefactory/petmanager/g12/petmanager_backend/supplier/controller/dto/SupplierRequestDTO.java
@@ -2,7 +2,6 @@ package com.codefactory.petmanager.g12.petmanager_backend.supplier.controller.dt
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
