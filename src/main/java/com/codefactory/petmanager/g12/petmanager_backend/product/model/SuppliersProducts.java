6b349bcf65eddf97419c9a7e020aba29ff0a4330
FelipeUH <f.uribe@udea.edu.co>
2025-11-18 10:05:44 -0500

    feature: add SuppliersProducts entity

---
+++ src/main/java/com/codefactory/petmanager/g12/petmanager_backend/product/model/SuppliersProducts.java
@@ -0,0 +1,36 @@
+package com.codefactory.petmanager.g12.petmanager_backend.product.model;
+
+import com.codefactory.petmanager.g12.petmanager_backend.supplier.model.Supplier;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "suppliers_products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SuppliersProducts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    @JoinColumn(name = "supplier_id", nullable = false)
+    private Supplier supplier;
+
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+    
+}
